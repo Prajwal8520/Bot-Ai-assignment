d5a24a5582e64f60d7355d45f26dcfe83b959e50
Prajwal8520 <141753318+Prajwal8520@users.noreply.github.com>
2023-08-08 21:25:45 +0545

    Add files via upload

---
+++ bot/chat012/information.py
@@ -0,0 +1,13 @@
+# message Greeting
+greeting = ["Hello sir, Please tell me how may I help you?","Nice to meet you?, Is there anything I can help you with?","Hi I'm google, How can I help you?","How can I help you?, Sir"]
+
+
+close =  ["Sad to see you go :(", "Talk to you later", "Goodbye! See you again", "Goodbye, Have a Nice Day."]
+ 
+news = ["Prajwal Shrestha is New Priminister of Nepal", "Barbi demolished Openhimer","Vice-President of Nepal suffers from fever kina ki mero desh birami","Heavy Rain pours throughout the country today"]
+
+temp = [ '19°C', '20°C',' 21°C', '24°C', '26°C ']
+
+breakfast=["bread jam", "Omlet and bread", "Chapati and milk tea", "Coffee with bread and butter"]
+
+plans=["picnic this Satarday", "welcome next Satardey", "2nd accesment from Bhadra 1"]
